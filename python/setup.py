8d6b6f00b8f2c721c1778a62dd277c6f8795d473
AS-MAC-0272 <varshith.gudur17@gmail.com>
2025-12-26 12:25:54 +0530

    python: update setup.py to use setuptools-rust

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,16 @@
-# Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
 from setuptools import setup, find_packages
+from setuptools_rust import Binding, RustExtension
 
 setup(
     name="valori",
     version="0.1.0",
     packages=find_packages(),
+    rust_extensions=[
+        RustExtension("valori.valori_ffi", path="../ffi/Cargo.toml", binding=Binding.PyO3),
+    ],
     install_requires=[
         "requests>=2.25.0",
     ],
+    setup_requires=["setuptools-rust>=1.5.0"],
+    zip_safe=False,
 )
