c95e6f43b084cd82be23bfe2c9fb6b00606b3802
AS-MAC-0272 <varshith.gudur17@gmail.com>
2026-06-10 10:57:13 +0530

    chore: apply dual MIT OR Apache-2.0 license to node/src/events/event_proof.rs

--- node/src/events/event_proof.rs
+++ node/src/events/event_proof.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
+// Copyright (c) 2025 Varshith Gudur. Dual-licensed under MIT OR Apache-2.0.
 //! Event Proof - Audit Trail Generation
 //!
 //! This module generates cryptographic proofs of system state
