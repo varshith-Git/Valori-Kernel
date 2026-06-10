3e26e73022d78582846058a6fb3b66bd677e1621
AS-MAC-0272 <varshith.gudur17@gmail.com>
2026-06-10 10:57:14 +0530

    chore: apply dual MIT OR Apache-2.0 license to node/src/persistence.rs

--- node/src/persistence.rs
+++ node/src/persistence.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 use crate::config::{IndexKind, QuantizationKind};
 use std::fs::File;
 use std::io::Write;
-// Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
+// Copyright (c) 2025 Varshith Gudur. Dual-licensed under MIT OR Apache-2.0.
 use std::path::Path;
 use crc32fast::Hasher;
 
