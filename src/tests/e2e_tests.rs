1e2001bd3185028602fdbf3bfd128894450c45f8
AS-MAC-0272 <varshith.gudur17@gmail.com>
2026-06-10 10:57:18 +0530

    chore: apply dual MIT OR Apache-2.0 license to src/tests/e2e_tests.rs

--- src/tests/e2e_tests.rs
+++ src/tests/e2e_tests.rs
@@ -2,7 +2,7 @@
 #![allow(dead_code)]
 extern crate std;
 use std::vec::Vec;
-// Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
+// Copyright (c) 2025 Varshith Gudur. Dual-licensed under MIT OR Apache-2.0.
 use crate::state::kernel::KernelState;
 use crate::state::command::Command;
 use crate::types::id::{RecordId, NodeId, EdgeId};
