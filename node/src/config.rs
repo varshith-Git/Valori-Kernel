4c51c202ae69735ca00ac39b203a4f986da87f9c
AS-MAC-0272 <varshith.gudur17@gmail.com>
2026-06-10 10:57:13 +0530

    chore: apply dual MIT OR Apache-2.0 license to node/src/config.rs

--- node/src/config.rs
+++ node/src/config.rs
@@ -1,7 +1,7 @@
-// Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
+// Copyright (c) 2025 Varshith Gudur. Dual-licensed under MIT OR Apache-2.0.
 use std::net::SocketAddr;
 use std::path::PathBuf;
-// Copyright (c) 2025 Varshith Gudur. Licensed under AGPLv3.
+// Copyright (c) 2025 Varshith Gudur. Dual-licensed under MIT OR Apache-2.0.
 use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Serialize, Deserialize)]
